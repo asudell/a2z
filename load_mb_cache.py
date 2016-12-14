33cd02c67674ca68046c2d295f41db09ab7a4a15
Drew Sudell <asudell@lifeshield.com>
2016-12-14 01:14:34 -0500

    Fix up MusicBrainz integration.  Yea! Bring back analysis on release year. Rerun for data. Publish though we're only half way through catching up the muiscbrainz data.

--- load_mb_cache.py
+++ load_mb_cache.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python
 
-from lxml import html
-import requests
 import os.path as path
-import csv
 import pandas as pd
 import musicbrainzngs as mb
 
@@ -11,80 +8,101 @@ cache_dir = './cache'
 xpn_cache = path.join(cache_dir, 'xpn')
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-letters_so_far = list(alphabet)[:19]
+letters_so_far = list(alphabet)[:20]
 
-rows = []
+# Load up the play lists
+playlist = pd.DataFrame(None, columns = ('Title', 'Artist'))
 for letter in letters_so_far:
     cache_file = path.join(xpn_cache, '%s.csv' % letter)
-    if not path.exists(cache_file):
-        with open(cache_file, 'wb') as csvfile:
-            writer = csv.writer(csvfile)
-            page = requests.get('http://xpn.org/static/az.php?q=%s' %  letter)
-            tree = html.fromstring(page.content)
-            plays = tree.xpath('//li/text()')
-            for play in plays:
-                writer.writerow(play.split(' - ', 1))
-                rows.append(play.split(' - ', 1))
-    else:
-        with open(cache_file, 'rb') as csvfile:
-            reader = csv.reader(csvfile)
-            for play in reader:
-                rows.append(play)
-playlist = pd.DataFrame(rows, columns=('Artist', 'Title'))
+    if path.exists(cache_file):
+        df = pd.read_csv(cache_file)
+        
+    playlist = playlist.append(df, ignore_index=True)
+
 print "playlist has %d rows" % len(playlist)
 
-mb_rows = []
+# Load MusicBrainz Data
 mb_cache = path.join(cache_dir, 'musicbrainz', 'song_years.csv')
 if path.exists(mb_cache):
-    with open(mb_cache, 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            mb_rows.append([row[0], row[1], int(row[2])])
-years = pd.DataFrame(mb_rows, columns=('Artist', 'Title', 'Year'))
+    years = pd.read_csv(mb_cache)
+else:
+    years = pd.DataFrame(None, columns=('Title','Artist', 'Year'))
+
 print "cached years has %d rows" % len(years)
 
-tmplist = playlist.join(years, how='left', rsuffix='_mb')
-tmplist = tmplist.drop(['Artist_mb', 'Title_mb'], 1)
+# merge the two data sets
+tmplist = playlist.merge(years, how='left')
 
+def fixup(name):
+    # Music Brainz is fussy about some anames
+    if "R. E. M." == name:
+        return "REM"
+    elif "B. T. Express" == name:
+        return "BT Express"
+    elif "J. J. Cale" == name:
+        return "JJ Cale"
+    elif "k. d. lang" == name:
+        return "kd lang"
+    elif "J. J. Jackson" == name:
+        return "JJ Jackson"
+    elif "B. B. King" == name:
+        return "BB King"
+    elif "K. T. Tunstall" == name:
+        return "KT Tunstall"
+    elif "R. L. Burnside" == name:
+        return "RL Burnside"
+    elif "The J. Geils Band" == name:
+        return "The J Giles Band"
+    else:
+        return name
+        
+# if there are any null values, query MuicBrainz
+# for those songs collect meta data on them
 if tmplist.isnull().any().any():
     mb.set_useragent('xpn-a2z',
                      '0.1',
                      'https://github.com/asudell/a2z')
     new_mb_rows = []
     for index, row in tmplist[tmplist['Year'].isnull()].iterrows():
-        result = mb.search_releases(row['Title'],
-                                    artist = row['Artist'],
-                                    limit=25)
+        result = mb.search_recordings(row['Title'],
+                                      artist = fixup(row['Artist']),
+                                      status = 'official',
+                                      strict = True,
+                                      limit = 25)
         rel_year = None
-        if result['release-list']:
-            for release in  result['release-list']:
-                if 'date' in release:
-                    y = int(release['date'].split('-')[0])
-                    if rel_year is None or rel_year > y:
-                        rel_year = y
+        for recording in result['recording-list']:
+            
+            if recording['release-list']:
+                for release in  recording['release-list']:
+                    if 'date' in release:
+                        y = int(release['date'].split('-')[0])
+                        if rel_year is None or rel_year > y:
+                            if y > 1900:
+                                # assume anything before then is a type
+                                # there are a few with missing digits
+                                rel_year = y
         if rel_year is not None:
-            new_mb_rows.append([row['Artist'], row['Title'], rel_year])
+            new_mb_rows.append([row['Title'], row['Artist'], rel_year])
 
-    if len(new_mb_rows) > 0:
-        print "fetched %d new rows" % len(new_mb_rows)
-        mb_rows.extend(new_mb_rows)
-        print "saving cache with %d rows" % len(mb_rows) 
-        with open(mb_cache, 'wb') as csvfile:
-            writer = csv.writer(csvfile)
-            for row in mb_rows:
-                writer.writerow(row)
+    print "fetched %d new rows" % len(new_mb_rows)
+    
+    new_years = pd.DataFrame(new_mb_rows, columns=('Title', 'Artist', 'Year'))
+    if len(new_years) > 0:
+        years = years.append(new_years, ignore_index=True)
+        print 'saving cache with %s rows' % len(years)
+        years['Year'] = years['Year'].astype(int)
+        years.to_csv(mb_cache, index=False)
                 
-    years = pd.DataFrame(mb_rows, columns=('Artist', 'Title', 'Year'))
-    playlist = playlist.join(years, how='left', rsuffix='_mb')
-    playlist = playlist.drop(['Artist_mb', 'Title_mb'], 1)
+
+    playlist = playlist.merge(years, how='left')
     playlist['Year'] = playlist['Year'].fillna(0.0).astype(int)
     
-else:
-    playlist = tmplist
 
-print playlist.describe()
 print "resulting playlist has %d missing years" \
     % len(playlist[playlist['Year'] == 0])
+print
+for index, row in playlist[playlist['Year'] == 0].iterrows():
+    print row['Title'], row['Artist']
         
         
     
--- load_xpn_cache.py
+++ load_xpn_cache.py
@@ -3,7 +3,7 @@
 from lxml import html
 import requests
 import os.path as path
-import csv
+import pandas as pd
 
 cache_dir = './cache'
 xpn_cache = path.join(cache_dir, 'xpn')
@@ -11,23 +11,23 @@ xpn_cache = path.join(cache_dir, 'xpn')
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letters_so_far = list(alphabet)[:20]
 
-rows = []
+playlist = pd.DataFrame(None, columns = ('Title', 'Artist'))
 for letter in letters_so_far:
     cache_file = path.join(xpn_cache, '%s.csv' % letter)
-    if not path.exists(cache_file):
-        with open(cache_file, 'wb') as csvfile:
-            writer = csv.writer(csvfile)
-            page = requests.get('http://xpn.org/static/az.php?q=%s' %  letter)
-            tree = html.fromstring(page.content)
-            plays = tree.xpath('//li/text()')
-            for play in plays:
-                writer.writerow(play.split(' - ', 1))
-                rows.append(play.split(' - ', 1))
+    if path.exists(cache_file):
+        df = pd.read_csv(cache_file)
     else:
-        with open(cache_file, 'rb') as csvfile:
-            reader = csv.reader(csvfile)
-            for play in reader:
-                rows.append(play)
+        rows = []
+        page = requests.get('http://xpn.org/static/az.php?q=%s' %  letter)
+        tree = html.fromstring(page.content)
+        songs = tree.xpath('//li/text()')
+        for song in songs:
+            rows.append(song.split(' - ', 1))
+        df = pd.DataFrame(rows, columns = ('Title', 'Artist'))
+        df.to_csv(cache_file, index=False)
 
-print "%d songs in %d letters" % (len(rows), len(letters_so_far))
+    playlist = playlist.append(df, ignore_index=True)
+    print "adding %d songs for running total of %d" % (len(df), len(playlist))
+    
+print "%d songs in %d letters" % (len(playlist), len(letters_so_far))
 
