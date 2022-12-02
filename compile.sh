dbe4b8a9a8cb4878767b86bea5bacabc8b25df97
Drew Sudell <drew@drewsudell.org>
2022-12-01 23:08:18 -0500

    Start adapting stuff to this years playlist. Start with getting the parse right for the new playlist format.

---
+++ compile.sh
@@ -0,0 +1,5 @@
+jupyter nbconvert --to html Dataloading2.ipynb
+#jupyter nbconvert --to hide_code_html XPN2020.ipynb
+#jupyter nbconvert --to hide_code_html XPN2020_One_Vote_Wonders.ipynb
+
+
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+FILES="Dataloading2.html"
+#FILES="$FILES XPN2020.html XPN2020_One_Vote_Wonders.html"
+#FILES="$FILES data/xpn2020.csv data/xpn2020_onsies.csv"
+#FILES="$FILES data/XPN2020_and_88Worst.csv data/XPN2020_and_885Best.csv"
+#FILES="$FILES data/885best_xpn2020_reranked.csv"
+DEST=regulus:/var/www/www.drewsudell.org/html/projects/a2z
+for x in ${FILES}
+	do scp $x ${DEST}/${x}
+done
+
+
