302b681c2aea23fb8663c0df433cfd710d6b198b
Drew Sudell <drew@drewsudell.org>
2023-12-05 17:14:21 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 FILES="Dataloading2.html"
 FILES="$FILES 885SongsByWomen.html"
 # XPN2020_One_Vote_Wonders.html"
-FILES="$FILES data/90sA2Z.csv"
+FILES="$FILES data/women.csv"
 #FILES="$FILES data/xpn2020_onsies.csv"
 #FILES="$FILES data/XPN2020_and_88Worst.csv data/XPN2020_and_885Best.csv"
 #FILES="$FILES data/885best_xpn2020_reranked.csv"
